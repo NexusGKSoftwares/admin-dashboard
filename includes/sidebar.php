3dc1616ff14efeaea618cb3ddaa44ac6f898221a
NexusGK Developer <nexusgk3@gmail.com>
2024-12-22 21:08:37 +0300

    `Refactor sidebar layout and styling`

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,11 +1,27 @@
-<aside class="sidebar">
-    <ul>
-        <li><a href="?page=dashboard">Dashboard</a></li>
-        <li><a href="?page=user_management">User Management</a></li>
-        <li><a href="?page=billing_management">Billing & Payments</a></li>
-        <li><a href="?page=service_management">Service Management</a></li>
-        <li><a href="?page=usage_monitoring">Usage Monitoring</a></li>
-        <li><a href="?page=notifications">Notifications</a></li>
-        <li><a href="?page=reports">Reports</a></li>
-    </ul>
+<aside class="sidebar bg-light" style="width: 250px; position: fixed; top: 0; left: 0; height: 100vh;">
+    <div class="container-fluid mt-4">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link" href="?page=dashboard">Dashboard</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?page=user_management">User Management</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?page=billing_management">Billing & Payments</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?page=service_management">Service Management</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?page=usage_monitoring">Usage Monitoring</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?page=notifications">Notifications</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?page=reports">Reports</a>
+            </li>
+        </ul>
+    </div>
 </aside>
