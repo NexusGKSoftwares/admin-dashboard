e2f8cdb5d3bb3dcd5a63317f12992bea5e872fb6
NexusGK Developer <nexusgk3@gmail.com>
2024-12-22 21:02:46 +0300

    `Added sidebar navigation menu with links to various pages.`

---
+++ includes/sidebar.php
@@ -0,0 +1,11 @@
+<aside class="sidebar">
+    <ul>
+        <li><a href="?page=dashboard">Dashboard</a></li>
+        <li><a href="?page=user_management">User Management</a></li>
+        <li><a href="?page=billing_management">Billing & Payments</a></li>
+        <li><a href="?page=service_management">Service Management</a></li>
+        <li><a href="?page=usage_monitoring">Usage Monitoring</a></li>
+        <li><a href="?page=notifications">Notifications</a></li>
+        <li><a href="?page=reports">Reports</a></li>
+    </ul>
+</aside>
